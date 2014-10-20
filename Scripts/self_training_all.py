6f8413603a446be748e3860b837e615870671d6e
claire <claireemjoyce@gmail.com>
2014-10-20 18:08:08 +0200

    fixed

--- Scripts/self_training_all.py
+++ Scripts/self_training_all.py
@@ -1,3 +1,5 @@
+import cPickle
+
 __author__ = 'claire'
 from utility import load_twitter_2class, load_amazon
 from sklearn.svm import LinearSVC as svc
@@ -33,34 +35,40 @@ clf = svc()
 clf.fit(X_train, y_train)
 # print clf.score(X_test,y_test)
 
+def totarget(i):
+    if i < 0:
+        result = -1
+    else:
+        result = 1
+    return result
 
+# train=np.array(train)
+
+
+print 'initial shapes (train, unlabeled)', X_train.shape, len(y_train), X_U.shape
 threshold = 0.5
-added = 0
+iters = 100
 scores = []  #keep track of how it changes according to the development set
-for i in range(X_U.shape[0]):
+scores.append(clf.score(X_test, y_test))
+for i in range(iters):
     # print clf.predict(i)
-    distance = clf.decision_function(X_U[i])  # the distance (- or +) from the hyperplane
-    if abs(distance) > threshold:
-        # print 'adding',U[i]
-        added += 1
-        train.append(U[i])
-        if distance > 0:
-            y_train.append(1)
-        else:
-            y_train.append(-1)
-        vec = tf()
+    distance = clf.decision_function(X_U)  # the distance (- or +) from the hyperplane
+    toadd = abs(distance) > threshold
+    temp = np.array(U)[toadd].tolist()
+    train = train + temp
+    target = map(totarget, distance)
+    y_train = y_train + np.array(target)[toadd].tolist()
     X_train = vec.fit_transform(train)
     X_test = vec.transform(test)
     X_U = vec.transform(U)
     clf.fit(X_train, y_train)
     if i % 10 == 0:
         scores.append(clf.score(X_test, y_test))
-        print 'added %d unlabeled datapoints' % added
+        print 'train set size: %d' % X_train.shape[0]
         print 'Iteration %d : accuracy: %f ' % (i, scores[-1])
-print clf.score(X_test, y_test)
-print added
-plt.plot(range(len(scores)), scores)
 
+print clf.score(X_test, y_test)
+plt.plot(range(len(scores)), scores)
 plt.xlabel('iters')
 plt.ylabel('accuracy')
 plt.show()
